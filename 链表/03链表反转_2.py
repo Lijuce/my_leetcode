f40fd3e3179750ce35a8b5f6d603594092e6b6a1
Lijuce <441214473@qq.com>
2021-03-10 11:26:49 +0800

    部分区间链表反转

---
+++ 链表/03链表反转_2.py
@@ -0,0 +1,107 @@
+# LeetCode题号92-反转链表2(连续区间部分链表反转)
+# python实现链表反转-连续区间部分链表反转
+
+class Node:
+    def __init__(self, val=0, next=None):
+        self.data = val
+        self.next = next
+
+class SingleLinkList(object):
+    def __init__(self):
+        self.head = 0  # 头结点
+    
+    def construct(self, datas: list):
+        # 单链表创建
+        self.head = Node(datas[0])
+        p = self.head
+        for number in datas[1:]:
+            new_node = Node(number)
+            p.next = new_node
+            p = p.next
+        p.next = None  # 尾部结点指向None
+        return self.head
+
+    def travel(self, head):
+        # 链表遍历
+        cur = head
+        while cur is not None:
+            print(cur.data)
+            cur = cur.next
+        # print(cur.data)  # 尾结点的值
+        print("遍历完成！")
+
+    def reverseBetween_PreN(self, head, N, mode="recu"):
+        # 递归方式实现反转链表前N个节点
+        # head = self.head
+        def Between(head: Node, right: int):
+            if right == 1:
+                global successor  # 全局变量声明，否则successor无法直接调用
+                successor = head.next
+                return head
+            last = Between(head.next, right-1)
+            head.next.next = head
+            head.next = successor  
+
+            return last
+        tmp_node = Between(head, N)
+
+        print("链表前N个节点反转完成")
+        return tmp_node
+
+    def reverseBetween(self, head, left, right):
+        # 递归方式实现反转链表指定区间内[m, n]节点
+        if left == 1:  # base case
+            tmp_node = self.reverseBetween_PreN(head=head, N=right)  # 当left=1时，相当于反转前N个元素
+            return tmp_node
+
+        last = self.reverseBetween(head.next, left-1, right-1)
+        head.next = last
+        print("链表区间个节点反转完成")
+        return head
+
+    def reverse(self, start: Node, obj: Node) -> Node:
+        # 迭代方式实现链表整体反转
+        cur = start
+        nxt = obj
+        pre = None
+        while cur != obj:  # 1 -> 2 -> 3
+            nxt = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nxt
+        head = pre
+        return head
+
+    def reverseKGroup(self, head: Node, K: int) -> Node:
+        # 链表中K元素一组进行反转
+        if head is None:
+            return None
+
+        start = head
+        obj = head
+
+        for i in range(K):
+            # 从头开始计目标节点所在位置
+            if obj is None:
+                return head  # base case: 当不足K个，无需反转
+
+            obj = obj.next
+
+        new_head = self.reverse(start, obj)
+
+        tmp_node = self.reverseKGroup(obj, K)
+        start.next = tmp_node  # 将前面组和下一组进行连接
+
+        return new_head
+
+
+SingleLink = SingleLinkList()
+link = SingleLink.construct([1,2,3,4,5,6,7])  # 链表初始化
+# link = SingleLink.reverseBetween_PreN(head=link, N=3, mode='recu')
+# link = SingleLink.reverseBetween(link, left=2, right=4)
+# SingleLink.travel(link)
+link = SingleLink.reverseKGroup(link, 3)
+# a = link.next
+# b = a.next.next
+# link = SingleLink.reverse(a, b)
+SingleLink.travel(link)
