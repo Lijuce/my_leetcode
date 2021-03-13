a82d18b1c21b304ddbb2a4982dc94a153fda400d
Lijuce <441214473@qq.com>
2021-03-13 14:22:14 +0800

    二叉树基本操作

--- 二叉树/01二叉树CRUD.py
+++ 二叉树/01二叉树CRUD.py
@@ -1,60 +1,76 @@
 # 二叉树的增、删、查、改
+from BiTree import *
 
-class Node:
-    def __init__(self, val):
-        self.data = val
-        self.left = None
-        self.right = None
+class Tree(BiTree):
+    def print_tree(self, root, mode='preorder'):
+        if mode == 'preorder':
+            print("前序遍历输出：", end='')
+            self.preorder(root)
+            print("")
+        elif mode == 'inorder':
+            print("中序遍历输出：", end='')
+            self.inorder(root)
+            print("")
+        elif mode == 'postorder':
+            print("后序遍历输出：", end='')
+            self.postorder(root)        
+            print("")
+        elif mode == 'BFS':
+            print("层次遍历输出：", end='')
+            self.BFS(root)
+            print("")
+        else:
+            raise ValueError("No the mode!")
 
-class BiTree(object):
-    def __init__(self):
-        self.root = None  # 二叉树的根初始化
+    def preorder(self, root):
+        # 前序遍历
+        if root is None:
+            return
+        if root.data != '#':
+            print(root.data, end=' ')
+        self.preorder(root.left)
+        self.preorder(root.right)
+    
+    def inorder(self, root):
+        # 中序遍历
+        if root is None:
+            return
+        self.inorder(root.left)
+        if root.data != '#':
+            print(root.data, end=' ')
+        self.inorder(root.right)
 
-    def construct(self, datas: list):
-        # 二叉树的构建
-        self.root = Node(datas[0])  # 根节点
-        stack = [self.root]
-        for i in range(1, len(datas), 2):
-            if datas[i] != None:
-                left = Node(datas[i])
-            else:
-                left = None
+    def postorder(self, root):
+        # 后序遍历
+        if not root:
+            return
+        if root.data != '#':
+            print(root.data, end=' ')
+        self.postorder(root.left)
+        self.postorder(root.right)
+        print(root.data, end=' ')
 
-            if i+1 < len(datas) and datas[i+1] != None:
-                right = Node(datas[i+1])
-            else:
-                right = None
-            
-            peek = stack.pop()
-            peek.left = left
-            peek.right = right
-            stack.append(peek.right)
+    def BFS(self, root):
+        # 层次遍历
+        if root is None:
+            return
 
-        return self.root
+        stack = [root]
+        while stack:
+            peek = stack.pop(0)
+            if peek is None:
+                return
+            right = peek.right
+            left = peek.left
+            # print(left.data)
+            # print(right.data)
+            print(peek.data, end=' ')
 
-    def print_tree(self, root):
-        if root == None:
-            return []
-        result = []
-        queue = []
-        queue.append(root)
-        while len(queue) != 0:
-            peek = queue.pop()
-            if peek == None:
-                if len(queue) != 0:
-                    result.append(None)
-            else:
-                result.append(peek.data)
+            stack.append(left)
+            stack.append(right)
 
-                if peek.right != None or peek.left != None:
-                    queue.append(peek.right)
-                    queue.append(peek.left)
-        print(result)
-        return result
-
-tree = BiTree()
-nums = [1, 3, 2, 5]
+tree = Tree()
+nums = [1, 2, 3, 4, 5, 6, 7, '#', '#', '#', '#', '#', '#', '#', '#']
 root = tree.construct(nums)
-tree.print_tree(root) 
-
-
+tree.print_tree(root)
+tree.print_tree(root, mode='inorder')
