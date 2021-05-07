714a9329f5f1cec13cafa4a238ce6412ff2ef14f
Lijuce <441214473@qq.com>
2021-05-07 23:00:50 +0800

    链表反转1-java

---
+++ 链表/206链表反转.java
@@ -0,0 +1,36 @@
+public class reverseLink {
+
+    /**
+     * 迭代法反转单链表
+     * @param head
+     * @return
+     */
+    public static LinkNode reverseSingleLinkIter(LinkNode head){
+        LinkNode cur = head;
+        LinkNode prev = null;
+        LinkNode back;
+        while (cur != null){
+            back = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = back;
+        }
+        head = prev;
+        return head;
+    }
+
+    /**
+     * 递归反转链表
+     * @param head
+     * @return
+     */
+    public static LinkNode reverseSingleLinkRec(LinkNode head){
+        if (head.next == null){
+            return head;
+        }
+        LinkNode last = reverseSingleLinkRec(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+}
