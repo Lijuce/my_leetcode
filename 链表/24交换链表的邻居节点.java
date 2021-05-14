a5d78e912bba732e84cb3eb2791bef89b4836eca
Lijuce <441214473@qq.com>
2021-05-14 10:40:36 +0800

    两两交换链表中的节点-java

---
+++ 链表/24交换链表的邻居节点.java
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * 24. 两两交换链表中的节点
+     * 穿针引线的迭代法
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs(ListNode head) {
+        // 特判
+        if (head == null || head.next == null)
+            return head;
+
+        // 用dummyNode记住头节点
+        ListNode dummyNode = new ListNode(-1);
+        ListNode curNode = dummyNode;
+        dummyNode.next = head;
+
+        ListNode p1 = null;
+        ListNode p2 = null;
+        while (curNode.next !=null && curNode.next.next != null){
+            p1 = curNode.next;
+            p2 = p1.next;
+
+            // 采用经典的穿针引线法，3步
+            p1.next = p2.next;
+            p2.next = p1;
+            curNode.next = p2;
+
+            // 循环遍历更新
+            curNode = p1;
+        }
+    }
+}
+
