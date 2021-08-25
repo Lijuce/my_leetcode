89d2b3a0cc0efa24a72829fe06506714310850c9
Lijuce <441214473@qq.com>
2021-08-25 19:32:35 +0800

    最长回文子串-增动态规划解法（原本是暴力破解方式）

--- 关于动态规划/44最长回文子串.java
+++ 关于动态规划/44最长回文子串.java
@@ -31,3 +31,46 @@ class Solution {
     }
 }
 
+class Solution2 {
+    /**
+     * 二维数组-DP动态规划
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        // 定义DP: dp[i][j]表是否回文串
+        boolean[][] dp = new boolean[len][len];
+        // 初始化(二维表格的对角线)
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        // 定义最长回文串的长度
+        int maxLen = 1;
+        // 回文串起始位置
+        int begin = 0;
+        // 三种情况：参考回文子串
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i+1][j-1];
+                    }
+                }
+                if (dp[i][j] && j-i+1 > maxLen) {
+                    maxLen = j-i+1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+}
