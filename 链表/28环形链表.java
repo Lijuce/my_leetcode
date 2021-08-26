80ed879b133d0ae4bacde692da554bcd7d3cd530
Lijuce <441214473@qq.com>
2021-08-26 17:37:49 +0800

    环形链表-双指针

---
+++ 链表/28环形链表.java
@@ -0,0 +1,21 @@
+public class Solution {
+    /**
+     * 141. 环形链表
+     * 快慢指针
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow)
+                return true;
+        }
+        return false;
+    }
+}
