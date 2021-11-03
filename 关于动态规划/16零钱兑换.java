59df325ff8df9d7b12cefdc5cc9ba0714fd4a3c8
Lijuce <441214473@qq.com>
2021-11-03 22:53:09 +0800

    动态规划之零钱兑换I-优化

--- 关于动态规划/16零钱兑换.java
+++ 关于动态规划/16零钱兑换.java
@@ -5,17 +5,19 @@ class Solution {
      */
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount+1];
-        for (int i = 0; i < dp.length; i++)
+        // 初始化为不可能达到的最大值，方便后续的最小值动归优化
+        for (int i = 0; i < amount+1; i++) {
             dp[i] = amount+1;
+        }
         dp[0] = 0;
-        for(int i = 1; i < dp.length; i++){
-            for (int c: coins){
-                if (i - c < 0)
-                    continue;
-                dp[i] = Math.min(dp[i], dp[i-c] + 1);
+        for (int i = 1; i < amount+1; i++) {
+            for (int c: coins) {
+                if (i >= c) {
+                    dp[i] = Math.min(dp[i], dp[i-c] + 1);
+                }
             }
         }
-        return (dp[amount] == amount+1) ? -1: dp[amount];
-
+        // 进行判断：若最终值与初始值一致，则表明无解，返回-1。
+        return dp[amount] == amount+1? -1: dp[amount];
     }
 }
